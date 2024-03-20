3016abe3802dc8d27009fa146752093ddaaed336
StefaCoder <stefa1104@gmail.com>
2024-03-20 12:36:57 -0400

    Add warning popup message to confirm delete all players action

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,23 +6,35 @@ require('dotenv').config()
 const Footer = () => {
     const [error, setError] = useState('')
     const [isPlayerAdded, setIsPlayerAdded] = useState(true)
+    const [isConfirmDelAllMsg, setIsConfirmDelAllMsg] = useState(false)
 
     const fetchAllPlayers = useContext(PlayerDataContext)
 
 
-    const handleDeleteAll = async (e) => {
+    const handleDeleteAll = (e) => {
         e.preventDefault()
-        
+        setIsConfirmDelAllMsg(true)
+    }
+
+    const handleYesAction = async (e) => {
+        e.preventDefault()
+
         try{
             const response = await axios.delete(`${process.env.DELETE_ALL_PLAYER_API_URL}`)
             console.log("Players deleted", response.status)
             setIsPlayerAdded(false)
+            setIsConfirmDelAllMsg(false)
             fetchAllPlayers()
 
         }catch (error) {
             console.error('The following error has occurred: ', error.message)
             setError("Delete All failed: " + error.message)
+            setIsConfirmDelAllMsg(false)
         }
+    }
+
+    const handleNoAction = (e) => {
+        setIsConfirmDelAllMsg(false)
     }
 
     const handleDelAllIcon = (e) => {
@@ -33,6 +45,13 @@ const Footer = () => {
 
     return(
         <div>
+            {isConfirmDelAllMsg && 
+            <div className="confirmDelAllActionDiv">
+                <h4>Are you sure? <span className="material-symbols-outlined">warning</span></h4>
+                <p>This action will delete all players permanently!</p>
+                <button className="btnYes" onClick={handleYesAction}>YES</button>
+                <button className="btnNo" onClick={handleNoAction}>NO</button>
+            </div>}
             <div className="deleteAllMsgs">
             {!isPlayerAdded && <div className="noPlayersMsgDiv">No players to display <span className="material-symbols-outlined" onClick={handleDelAllIcon}>close</span></div>}
             {error && <div className="delAllMsgDiv">{error} <span className="material-symbols-outlined" onClick={handleDelAllIcon}>close</span></div>}
